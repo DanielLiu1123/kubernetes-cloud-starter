3df1e3192a77576856fe9045f865862d81f9869b
Freeman Liu <llw599502537@gmail.com>
2022-11-27 19:01:12 +0800

    feat: 3.0.x

--- kubernetes-config-cloud-starter/src/main/java/com/freemanan/kubernetes/config/KubernetesConfigAutoConfiguration.java
+++ kubernetes-config-cloud-starter/src/main/java/com/freemanan/kubernetes/config/KubernetesConfigAutoConfiguration.java
@@ -4,17 +4,17 @@ import com.freemanan.kubernetes.config.core.ConfigWatcher;
 import com.freemanan.kubernetes.config.util.KubernetesUtil;
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Freeman
  */
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration
 @ConditionalOnClass({KubernetesClient.class, ConfigMap.class})
 @ConditionalOnProperty(prefix = KubernetesConfigProperties.PREFIX, name = "enabled", matchIfMissing = true)
 @EnableConfigurationProperties(KubernetesConfigProperties.class)
